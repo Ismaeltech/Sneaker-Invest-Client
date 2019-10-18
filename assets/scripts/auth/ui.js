e9d948f51bc0dfb72459167deeee809a8bed13f6
ismaeltech <edy1017@live.com>
2019-10-18 11:41:20 -0400

    Added auth forms & create sneaker form
    Started with the html by adding the proper forms.
    Auth files added app/events/api/ui.js

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,74 @@
+'use strict'
+const store = require('./../store')
+
+const successMessage = function (newText) {
+  $('#message').text(newText)
+  $('#message').removeClass('failure')
+  $('#message').addClass('success')
+  $('form').trigger('reset')
+}
+
+const failureMessage = function (newText) {
+  $('#message').text(newText)
+  $('form').trigger('reset')
+  $('#message').addClass('failure')
+  $('#message').removeClass('success')
+}
+
+const onSignUpSuccess = responseData => {
+  successMessage('Signed up successfully!')
+  $('#message').css('color', 'green')
+  $('#sign-up').hide()
+}
+
+const onSignUpFailure = () => {
+  failureMessage('Sign up failed')
+  $('#message').css('color', 'red')
+}
+
+const onSignInSuccess = function (response) {
+  successMessage('Signed in successfully')
+  store.user = response.user
+  $('#message').css('color', 'green')
+  $('#sign-up, #sign-in').hide()
+  $('#change-password').show()
+  $('#sign-out').show()
+}
+
+const onSignInFailure = function () {
+  failureMessage('Sign in failed')
+  $('#message').css('color', 'red')
+}
+
+const onChangePasswordSuccess = function () {
+  successMessage('Changed password successfully!')
+  $('#message').css('color', 'green')
+}
+
+const onChangePasswordFailure = function () {
+  failureMessage('change password failed')
+  $('#message').css('color', 'red')
+}
+
+const onSignOutSuccess = responseData => {
+  successMessage('Signed out successfully!')
+  $('#sign-in').show()
+  $('#sign-up').show()
+  $('#change-password').hide()
+  $('#sign-out').hide()
+}
+
+const onSignOutFailure = function () {
+  failureMessage('Sign out failed')
+  $('#message').css('color', 'red')
+}
+module.exports = {
+  onSignUpSuccess,
+  onSignUpFailure,
+  onSignInSuccess,
+  onSignInFailure,
+  onChangePasswordSuccess,
+  onChangePasswordFailure,
+  onSignOutSuccess,
+  onSignOutFailure
+}
